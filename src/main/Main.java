54278e7fcb92bc8f254c8d0c9107f6ebf28b943a
Madis <madislugus@gmail.com>
2015-11-17 17:42:35 +0200

    *Programmi täielik alge *peamiselt javaFX'iga mässamine *loodud nupud *menüü ning selle sisu kus ka teised menüüd
    VEAD:
    *layout tuleb korda teha

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,46 +1,84 @@
 package main;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    Button button;
+    Button looGrupp, valiGrupp;
+    Scene scene;
+    GridPane layout;
+    MenuBar topMenu;
+    Menu fileMenu, looMenu, kustutaMenu;
 
     public static void main(String[] args) {
-    launch(args);
+        launch(args);
     } // main.Main meetodi l6pp
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        // menu loomine jne.
+        fileMenu = new Menu("_File");
+        looMenu = new Menu("Lisa/Loo");
+        kustutaMenu = new Menu("Kustuta");
+
+
+        // fileMenu osad e. peamine menüü "file"
+        fileMenu.getItems().addAll(looMenu, kustutaMenu);
+        fileMenu.getItems().add(new SeparatorMenuItem());
+        MenuItem j2lgi = new MenuItem("J2lgi kulutusi...");
+        fileMenu.getItems().add(j2lgi);
+        MenuItem jaga = new MenuItem("Jaga raha...");
+        fileMenu.getItems().add(jaga);
+
+        // looMenu osad e. pea menu sees
+        MenuItem lisaLiige = new MenuItem("Lisa uus liige...");
+        looMenu.getItems().add(lisaLiige);
+        MenuItem looGrupp1 = new MenuItem("Loo uus grupp");
+        looMenu.getItems().add(looGrupp1);
+
+        // kustutaMenu osad e. pea menu sees
+        MenuItem kustutaGrupp = new MenuItem("Kustuta grupp...");
+        kustutaMenu.getItems().add(kustutaGrupp);
+        MenuItem kustutaLiige = new MenuItem("Kustuta liige...");
+        kustutaMenu.getItems().add(kustutaLiige);
+
+        // main menu bar
+        topMenu = new MenuBar();
+        topMenu.getMenus().addAll(fileMenu);
+        GridPane.setConstraints(topMenu, 0, 0);
+
+        //main layout
+        layout = new GridPane();
+        layout.setPadding(new Insets(0, 10, 10, 0));
+        layout.setVgap(8);
+        layout.setHgap(10);
+
+        // tervituskiri
+        Label tervitus = new Label("Tere tulemast MoneySplitter'isse!!");
+        GridPane.setConstraints(tervitus, 8, 3);
+
+        // loo uus grupp nupp
+        looGrupp = new Button("Loo uus grupp");
+        GridPane.setConstraints(looGrupp, 8, 12);
+
+        // vali grupp nupp
+        valiGrupp = new Button("Vali grupp");
+        GridPane.setConstraints(valiGrupp, 8, 15);
+
+        //lisan kõik osad layouti
+        layout.getChildren().addAll(valiGrupp, looGrupp, tervitus, topMenu);
+
+        // scene1 ja window
         primaryStage.setTitle("MoneySplitter");
-
-        Button nupp = new Button("vajuta mind");
-        Button nupp2 = new Button("mine tagasi");
-
-        Label label = new Label("mingi kiri");
-
-        StackPane layout = new StackPane();
-        layout.getChildren().add(nupp);
-
-        VBox layout1 = new VBox();
-        layout1.getChildren().addAll(nupp2, label);
-
-        Scene scene = new Scene(layout, 300, 250);
+        scene = new Scene(layout, 350, 475);
         primaryStage.setScene(scene);
         primaryStage.show();
-
-        Scene scene1 = new Scene(layout1, 300, 250);
-
-        nupp.setOnAction(event -> primaryStage.setScene(scene1));
-        nupp2.setOnAction(event -> primaryStage.setScene(scene));
 
 
     }
